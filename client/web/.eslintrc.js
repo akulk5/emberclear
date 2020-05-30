a0eea4d88ff056bfeec1e8c57de09fe2e781251a
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-05-30 15:42:23 -0400

    eh

---
+++ client/web/.eslintrc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// This configuration doesn't matter.
+// Projcets are overwrite with their own config.
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+};
