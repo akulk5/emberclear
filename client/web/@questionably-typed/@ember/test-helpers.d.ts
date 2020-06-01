b05bbaf9e9d43e33d04accf7105ae3a4d3a27a31
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-05-31 22:50:36 -0400

    configuring this is blegh

---
+++ client/web/@questionably-typed/@ember/test-helpers.d.ts
@@ -0,0 +1,14 @@
+interface Owner {
+  lookup: <T>(name: string) => T;
+  register: <T>(name: string, mockService: T) => void;
+}
+export interface AppContext {
+  element: HTMLElement;
+  owner: Owner & {
+    application: {
+      inject: (within: string, name: string, injected: string) => void;
+    };
+  };
+}
+
+export function getContext(): AppContext;
